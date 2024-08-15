3a73b606c62f8f72a4a12a9cf4f86227e1f195bb
Anand Shaurya <shaurya252anand@gmail.com>
2024-08-15 20:36:53 +0530

    Added Jinja2 templating.

--- app/main.py
+++ app/main.py
@@ -1,10 +1,16 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
 from app.routes import user
 
 app = FastAPI()
 
 app.include_router(user.router, prefix="/api")
 
+templates = Jinja2Templates(directory="templates")
+
 @app.get("/")
-async def read_root():
-    return {"message": "FastAPI project made by Anand Shaurya"}
+async def read_root(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
+
+app.mount("/static", StaticFiles(directory="static"), name="static")
